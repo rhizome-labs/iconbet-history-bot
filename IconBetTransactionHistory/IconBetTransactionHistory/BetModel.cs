1414803ebe9763287141da58b71fba43a48ac220
Andrew Burns <a_t_burns@hotmail.com>
2019-08-06 07:13:12 +0800

    adding Telegram IconbetHistoryBot Beta v0.02

---
+++ IconBetTransactionHistory/IconBetTransactionHistory/BetModel.cs
@@ -0,0 +1,14 @@
+﻿
+public class BetModel
+{
+    public string method { get; set; }
+    public Params param { get; set; }
+}
+
+public class Params
+{
+    public string numbers { get; set; }
+    public int color { get; set; }
+    public int even_odd { get; set; }
+    public string user_seed { get; set; }
+}
---
+++ IconBetTransactionHistory/IconBetTransactionHistory/NumericHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IconBetTransactionHistory
+{
+    public static class NumericHelper
+    {
+       public static string Loop2ICX(string bigInt, int wholeNumberLength, bool trimTrailing = true)
+        {
+            var result = string.Empty;
+            var str = bigInt;
+
+            if(wholeNumberLength >= 0)
+            {
+                str = str.PadLeft(wholeNumberLength, '0');
+                var wholeNum = str.Substring(0, wholeNumberLength);
+                var floatNum = str.Length > wholeNumberLength ? str.Substring(wholeNumberLength) : "0";
+                result = wholeNum + "." + floatNum;
+                result = result.TrimStart(new char[] { '0' });
+            }
+
+
+            if (trimTrailing)
+            {
+                result = result.TrimEnd(new char[] { '0' });
+            }
+
+            if (result.EndsWith("."))
+            {
+                result += "0";
+            }
+
+            return result;
+        }
+    }
+}
